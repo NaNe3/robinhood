808a68d207b332c9bdefc84beb8423b1e804a219
Eli Summers <elithedev@gmail.com>
2023-12-29 09:28:42 -0700

    finished asthetic terms at end of doc

--- fr/src/pages/modules/Investing.jsx
+++ fr/src/pages/modules/Investing.jsx
@@ -28,7 +28,7 @@ function Investing() {
                 <div className='info-container'>
                     <div className='buying-power'>
                         <p>Buying power</p>
-                        <p>$19.01<svg fill="black" height="16" role="img" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg"><path clip-rule="evenodd" d="m1.715 6.285 1.237-1.237L8 10.096l5.048-5.048 1.238 1.237L8 12.571 1.715 6.285Z" fill="var(--rh__text-color)" fill-rule="evenodd"></path></svg></p>
+                        <p>$19.01<svg style={{marginLeft: "10px"}} fill="black" height="16" role="img" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg"><path clip-rule="evenodd" d="m1.715 6.285 1.237-1.237L8 10.096l5.048-5.048 1.238 1.237L8 12.571 1.715 6.285Z" fill="var(--rh__text-color)" fill-rule="evenodd"></path></svg></p>
                     </div>
                 </div>
 
@@ -37,6 +37,11 @@ function Investing() {
                 <Trending />
                 <News />
                 <Articles />
+
+                <div className='invest-bottom'>
+                    <p>For more information, see our <a>Disclosures</a> and <a>Privacy Policy</a></p>
+                    <p>Options are risky and aren’t suitable for all investors. To learn more, read the <a>Options Disclosure Document</a></p>
+                </div>  
             </div>
 
             <Sidebar stocks={stocks} crypto={crypto} lists={lists} />
