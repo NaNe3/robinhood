235d0035ced916f88177b144e762074bb0a3aac0
Eli Summers <elithedev@gmail.com>
2024-01-05 14:27:05 -0700

    Daily Stock updates finished

--- bk/server.mjs
+++ bk/server.mjs
@@ -8,6 +8,37 @@ const port = 3000
 app.use(Cors())
 app.use(BodyParser.json())
 
+function dailyUpdate() {
+    console.log("=")
+    console.log("Daily Update Triggered")
+    console.log("-----------------------")
+    
+    let tickerSchema = Mongoose.Schema({
+        ticker: String,
+        name: String,
+        prices: [Number]
+    })
+
+    let ticker = Mongoose.model("ticker", tickerSchema)
+    ticker.find()
+        .then(tickers => {
+            tickers.forEach(tick => {
+                // let newest = tick.prices[tick.prices.length - 1]
+                // tick.prices.push((newest < 1 ?
+                //     Number((newest * (Math.random() * 0.2 + 0.9)).toFixed(5)) :
+                //     Number((newest * (Math.random() * 0.2 + 0.9)).toFixed(2))
+                // ))
+                // ticker.updateOne({ _id: tick._id }, { prices: ticker.prices })
+                //     .catch((err) => console.log(err));
+
+                // UNCOMMENT THIS WHEN YOU ARE READY TO BUILD APP
+
+                console.log("------------ SIMULAITNG ACTUAL TICKER UPDATE -----------")
+            })
+        })
+        .catch(err => console.log(err))
+}
+
 const url =  "mongodb+srv://admin:admin@godfather.gpqepzo.mongodb.net/?retryWrites=true&w=majority"
 Mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log("Connected to MongoDB"))
@@ -23,6 +54,39 @@ const Schema = Mongoose.Schema({
     lists: Array
 })
 const user = Mongoose.model("user", Schema)
+
+
+// METADATA RETRIEVE
+const metaSchema = Mongoose.Schema({
+    name: String,
+    last: String
+})
+const meta = Mongoose.model("metadata", metaSchema, "metadata")
+meta.find({name: "metadata"})
+    .then(data => data[0])
+    .then(result => {
+        const date = new Date();
+        const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        const today = monthNames[date.getMonth()] + ' ' + date.getDate();
+        
+        if (result.last != today) {
+            meta.updateOne({name: "metadata"}, {last: today})
+                .then(data => {
+                    console.log("Update Stocks Triggered")
+
+                    // CALL STOCK UPDATING FUNCTION HERE
+                    dailyUpdate()
+                })
+                .catch(err => console.log(err))
+
+        } else {
+            console.log("Stocks already updated today")
+        }
+    })
+    .catch(err => console.log(err))
+
+
+// METADATA END
 
 app.get("/login", (req, res) => {
     const {email, password} = req.query
